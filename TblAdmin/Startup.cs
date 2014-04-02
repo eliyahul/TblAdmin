87901f6e7c261046dfd80e11819eb7f537bf7ce0
Eliyahu <eliyahu@webconveyers.com>
2014-04-02 18:51:15 +0300

    Initial commit including .gitignore

---
+++ TblAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TblAdmin.Startup))]
+namespace TblAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
