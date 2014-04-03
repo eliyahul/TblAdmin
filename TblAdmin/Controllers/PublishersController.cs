d3efd800e339d63983f8e44563427a77734132a1
Eliyahu <eliyahu@webconveyers.com>
2014-04-03 18:17:15 +0300

    Scaffolded Books and Publishers

---
+++ TblAdmin/Controllers/PublishersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TblAdmin.Models;
+using TblAdmin.DAL;
+
+namespace TblAdmin.Controllers
+{
+    public class PublishersController : Controller
+    {
+        private TblAdminContext db = new TblAdminContext();
+
+        // GET: /Publishers/
+        public ActionResult Index()
+        {
+            return View(db.Publishers.ToList());
+        }
+
+        // GET: /Publishers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // GET: /Publishers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Publishers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name,Created,Modified")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Publishers.Add(publisher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(publisher);
+        }
+
+        // GET: /Publishers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: /Publishers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name,Created,Modified")] Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(publisher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
+
+        // GET: /Publishers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: /Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Publisher publisher = db.Publishers.Find(id);
+            db.Publishers.Remove(publisher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
