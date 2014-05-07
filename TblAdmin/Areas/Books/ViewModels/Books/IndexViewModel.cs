bfb9cf34254488b2771f27a08ad9349176067275
eliyahul <eliyahu@webconveyers.com>
2014-05-07 15:16:27 +0300

    Prep for refactoring BooksController, add a view model

---
+++ TblAdmin/Areas/Books/ViewModels/Books/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using TblAdmin.Areas.Books.Models;
+
+namespace TblAdmin.Areas.Books.ViewModels
+{
+    public class IndexViewModel
+    {
+        public string searchString { get; set; }
+        public string currentSortCol { get; set; }
+        public string currentSortOrder { get; set; }
+        public string nextSortOrder { get; set; }
+        public int? page { get; set; }
+        public int? pageSize { get; set; }
+        public PagedList.IPagedList<Book> books { get; set; }
+    }
+}
