8d11fa07587a09c04caf651c4aaffcd64d4a82c1
eliyahul <eliyahu@webconveyers.com>
2014-06-02 15:20:46 +0300

    cleanup

--- TblAdmin/Areas/Production/Controllers/ConversionController.cs
+++ TblAdmin/Areas/Production/Controllers/ConversionController.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.IO;
 using System.Globalization;
+using System.Text;
 
 namespace TblAdmin.Areas.Production.Controllers
 {
@@ -16,15 +11,20 @@ namespace TblAdmin.Areas.Production.Controllers
         static string bookName = "MangaTouch";
         static string publisherName = "Orca Currents";
         static string prefixPath = @"C:\Users\User\Documents\clients\Ronnie\Production\Books\";
-        static string publisherPartialPath = publisherName + @"\";
-        static string bookPartialPath = bookName + @"\";
-        static string filePath = prefixPath + publisherPartialPath + bookPartialPath + bookName + "_FullBook_EDITED-MANUALLY.txt";
-        static string fileString;
-        static string blankLine = Environment.NewLine + Environment.NewLine;
+        static string fileToWorkOn = bookName + "_FullBook_EDITED-MANUALLY.txt";
+        static string existingChapterHeading = "^chapter [a-z]{3,}";
+        string chapterHeadingLookahead = @"(?=chapter [a-z]{3,})";// positive lookahead to include the chapter headings
+            
+            
+        static string bookFolderPath = prefixPath + publisherName + @"\" + bookName + @"\";
+        string filePath = bookFolderPath + fileToWorkOn;
+        string fileString;
+        string blankLine = Environment.NewLine + Environment.NewLine;
 
         // GET: Production/Conversion
         public ActionResult Process()
         {
+            ViewBag.Results = "Success";
             bool fileExists = System.IO.File.Exists(filePath);
             if (!fileExists)
             {
@@ -32,15 +32,14 @@ namespace TblAdmin.Areas.Production.Controllers
                 return View();
             }
 
-            // Read file into a string
+            // Read file into a string and trim it.
             fileString = System.IO.File.ReadAllText(filePath, Encoding.GetEncoding(1252));
-
             fileString = fileString.Trim();
 
             // Standardize the chapter heading into Camel Case followed by period.
             fileString = Regex.Replace(
                 fileString, 
-                "^chapter [a-z]{3,}", 
+                existingChapterHeading, 
                 delegate(Match match)
                 {
                     TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
@@ -71,24 +70,17 @@ namespace TblAdmin.Areas.Production.Controllers
             
 
             // Split into files based on the Chapter headings
-            string chapterPartialPath = prefixPath + publisherPartialPath + bookPartialPath + "chapter-";
-            string chapterPartialPathNumbered = "";
-            string chapterPathTxt = "";
-            string chapterPathHtml = "";
-            string chapterHeadingPattern = @"(?=chapter [a-z]{3,})";// positive lookahead to include the chapter headings
-            
             int i = 0;
-            foreach (string s in Regex.Split(fileString, chapterHeadingPattern, RegexOptions.Multiline | RegexOptions.IgnoreCase))
+            foreach (string s in Regex.Split(fileString, chapterHeadingLookahead, RegexOptions.Multiline | RegexOptions.IgnoreCase))
             {
-                chapterPartialPathNumbered = chapterPartialPath + i.ToString("D3");
-                chapterPathTxt = chapterPartialPathNumbered + ".txt";
-                chapterPathHtml = chapterPartialPathNumbered + ".html";
+                string chapterPathTxt = bookFolderPath + "chapter-" + i.ToString("D3") + ".txt";
+                string chapterPathHtml = bookFolderPath + "chapter-" + i.ToString("D3") + ".html";
                 
                 if (i > 0)
                 {
                     string s_html = s.Trim();
 
-                    System.IO.File.WriteAllText(chapterPathTxt, s_html, Encoding.GetEncoding(1252));
+                    System.IO.File.WriteAllText(chapterPathTxt, s_html);
 
                     // Add html p tags to paragraph separations
                     s_html = Regex.Replace(
