043ed500a199c6efff3c654ba5375c4481711d5c
eliyahul <eliyahu@webconveyers.com>
2014-06-11 16:04:22 +0300

    Start writing tests against the conversion tool prototype and refactoring.

---
+++ TblAdmin.Tests/Controllers/ConversionControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Moq;
+using TblAdmin;
+using TblAdmin.Areas.Production.Controllers;
+using TblAdmin.DAL;
+using System.Data.Entity;
+using PagedList;
+
+namespace TblAdmin.Tests.Controllers
+{
+    [TestFixture]
+    public class ConversionControllerTest
+    {
+        private ConversionController controller;
+
+        static string bookNameRaw = "Uncle Vanya";
+        static string authorFirstNameRaw = "Anton";
+        static string authorLastNameRaw = "Chekhov";
+        static string publisherName = "Gutenberg";
+        static string prefixPath = @"C:\Users\User\Documents\clients\Ronnie\Production\Books\";
+        static string bookIdFromAdmin = "0000";
+        static string existingChapterHeading = "^chapter [a-zA-Z0-9:!\'?\", ]{1,}";
+        string chapterHeadingLookahead = @"(?=chapter [a-zA-Z0-9:!\'?"", ]{1,})";// positive lookahead to include the chapter headings
+        
+
+        [SetUp]
+        public void init()
+        {
+            controller = new ConversionController();
+        }
+
+        [Test]
+        [Ignore("Implementation for this test is not complete yet.")]
+        public void AA_example_ignored_testcase_to_keep_the_ignore_attribute_handy()
+        {
+        }
+
+        [Test]
+        public void Gives_message_for_nonexistant_file()
+        {
+            // Arrange
+            
+            string filePath = prefixPath + "my-non-existant-file";
+            string expectedResult = "Could not find file with pathname: " + filePath;
+
+
+            // Act
+            ViewResult result = controller.Process() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result.ViewBag.Results, expectedResult);
+            Assert.AreEqual(result.ViewBag.Results, expectedResult);
+        }
+    }
+}
