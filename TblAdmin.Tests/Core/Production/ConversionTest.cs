d9d718d8aa1cb5e55b2acdd910a1dfa8ca42a3d3
eliyahul <eliyahu@webconveyers.com>
2014-06-15 16:56:27 +0300

    Add tests and associated fixture files of conversion process - all pass

--- TblAdmin.Tests/Core/Production/ConversionTest.cs
+++ TblAdmin.Tests/Core/Production/ConversionTest.cs
@@ -19,20 +19,6 @@ namespace TblAdmin.Tests.Core.Production.Services
     {
         private Converter converter;
 
-        static string bookNameRaw = "Uncle Vanya";
-        static string authorFirstNameRaw = "Anton";
-        static string authorLastNameRaw = "Chekhov";
-        static string publisherName = "Gutenberg";
-        static string prefixPath = @"C:\Users\User\Documents\clients\Ronnie\Production\Books\";
-        static string bookIdFromAdmin = "0000";
-        static string existingChapterHeading = "^chapter [a-zA-Z0-9:!\'?\", ]{1,}";
-        static string chapterHeadingLookahead = @"(?=chapter [a-zA-Z0-9:!\'?"", ]{1,})";// positive lookahead to include the chapter headings
-        static string fileNameSuffix = "_FullBook_EDITED-MANUALLY.txt";
-
-        static string bookNameNoSpaces = Regex.Replace(bookNameRaw,  @"\s{0,}", "");
-        static string bookFolderPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\";
-        string filePath = bookFolderPath + bookNameNoSpaces + fileNameSuffix;
-        
         [SetUp]
         public void init()
         {
@@ -49,9 +35,22 @@ namespace TblAdmin.Tests.Core.Production.Services
         public void Gives_message_for_nonexistant_file()
         {
             // Arrange
+            string bookNameRaw = "Uncle Vanya";
+            string authorFirstNameRaw = "Anton";
+            string authorLastNameRaw = "Chekhov";
+            string publisherName = "Gutenberg";
+            string prefixPath = @"C:\Users\User\Documents\clients\Ronnie\Production\Books\";
+            string bookIdFromAdmin = "0000";
+            string existingChapterHeading = "^chapter [a-zA-Z0-9:!\'?\", ]{1,}";
+            string chapterHeadingLookahead = @"(?=chapter [a-zA-Z0-9:!\'?"", ]{1,})";// positive lookahead to include the chapter headings
+            string fileNameSuffix = "_FullBook_EDITED-MANUALLY.txt";
+
+            string bookNameNoSpaces = Regex.Replace(bookNameRaw,  @"\s{0,}", "");
+            string bookFolderPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\";
+            string filePath = bookFolderPath + bookNameNoSpaces + fileNameSuffix;
             filePath = prefixPath + "my-non-existant-file";
             string expectedResult = "Could not find file with pathname: " + filePath;
-            
+
 
             // Act
             Boolean result = converter.Convert(
@@ -66,7 +65,67 @@ namespace TblAdmin.Tests.Core.Production.Services
                 );
 
             // Assert
-            Assert.IsFalse (result);
+            Assert.IsFalse(result);
         }
+
+        [Test]
+        public void Converts_a_file_which_does_exist()
+        {
+            // Arrange
+            string bookNameRaw = "Uncle Vanya";
+            string authorFirstNameRaw = "Anton";
+            string authorLastNameRaw = "Chekhov";
+            string bookIdFromAdmin = "0000";
+            
+            string publisherName = "Gutenberg";
+            string fileNameSuffix = "_FullBook_EDITED-MANUALLY.txt";
+            string prefixPath = @"C:\Users\User\Documents\Visual Studio 2013\Projects\TblAdmin\TblAdmin.Tests\Core\Production\Fixtures\";
+            string bookNameNoSpaces = Regex.Replace(bookNameRaw, @"\s{0,}", "");
+            string actualResultsPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + "ActualResults" + @"\";
+            string expectedResultsPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + "ExpectedResults" + @"\";
+            string filePath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + bookNameNoSpaces + fileNameSuffix;
+            
+            string existingChapterHeading = "^chapter [a-zA-Z0-9:!\'?\", ]{1,}";
+            string chapterHeadingLookahead = @"(?=chapter [a-zA-Z0-9:!\'?"", ]{1,})";// positive lookahead to include the chapter headings
+            
+            
+            // Act
+            Boolean result = converter.Convert(
+                bookNameRaw,
+                authorFirstNameRaw,
+                authorLastNameRaw,
+                actualResultsPath,
+                filePath,
+                bookIdFromAdmin,
+                existingChapterHeading,
+                chapterHeadingLookahead
+                );
+
+            
+            // Assert
+            Assert.IsTrue(result);
+
+            IEnumerable<String> expectedFilePaths = System.IO.Directory.EnumerateFiles(expectedResultsPath);
+            foreach (string path in expectedFilePaths)
+            {
+                string expectedFileName = System.IO.Path.GetFileName(path);
+                string actualPath = actualResultsPath + expectedFileName;
+                Assert.IsTrue(System.IO.File.Exists(actualPath), "The following file does not exist in Actual Results: " + actualPath);
+
+                string expectedFileString = System.IO.File.ReadAllText(path);
+                string actualFileString = System.IO.File.ReadAllText(actualPath);
+
+                Assert.AreEqual(expectedFileString, actualFileString, " *** " + expectedFileName + " *** ");
+            }
+
+            // Tear Down (only runs if all Asserts pass, so if there is a failure, I can examine the file)
+            IEnumerable<String> actualFilePaths = System.IO.Directory.EnumerateFiles(actualResultsPath);
+            foreach (string path in actualFilePaths)
+            {
+                System.IO.File.Delete(path);
+            }
+
+        }
+
     }
 }
