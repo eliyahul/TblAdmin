7ddd4bbf118e796ca0f017b6e01abf515bd6bd1f
eliyahul <eliyahu@webconveyers.com>
2014-06-15 18:16:46 +0300

    Add test and fixture for part and chapter

--- TblAdmin.Tests/Core/Production/ConversionTest.cs
+++ TblAdmin.Tests/Core/Production/ConversionTest.cs
@@ -187,5 +187,66 @@ namespace TblAdmin.Tests.Core.Production.Services
 
         }
 
+        [Test]
+        public void Converts_a_novel_format_with_parts_and_chapters()
+        {
+            // Arrange
+            string bookNameRaw = "Anna Karenina";
+            string authorFirstNameRaw = "Leo";
+            string authorLastNameRaw = "Tolstoy";
+            string bookIdFromAdmin = "4425";
+
+            string publisherName = "Gutenberg";
+            string fileNameSuffix = "_FullBook_EDITED-MANUALLY-ABBREVIATED_FOR_TESTING.txt";
+            
+            string prefixPath = @"C:\Users\User\Documents\Visual Studio 2013\Projects\TblAdmin\TblAdmin.Tests\Core\Production\Fixtures\";
+            string bookNameNoSpaces = Regex.Replace(bookNameRaw, @"\s{0,}", "");
+            string actualResultsPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + "ActualResults" + @"\";
+            string expectedResultsPath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + "ExpectedResults" + @"\";
+            string filePath = prefixPath + publisherName + @"\" + bookNameNoSpaces + @"\" + bookNameNoSpaces + fileNameSuffix;
+
+            string existingChapterHeading = "^part [a-zA-Z0-9]{1,}: chapter [a-zA-Z0-9:!\'?\", ]{1,}";
+            string chapterHeadingLookahead = @"(?=part [a-zA-Z0-9]{1,}: chapter [a-zA-Z0-9:!\'?"", ]{1,})";// positive lookahead to include the chapter headings
+
+
+            // Act
+            Boolean result = converter.Convert(
+                bookNameRaw,
+                authorFirstNameRaw,
+                authorLastNameRaw,
+                actualResultsPath,
+                filePath,
+                bookIdFromAdmin,
+                existingChapterHeading,
+                chapterHeadingLookahead
+                );
+
+
+            // Assert
+            Assert.IsTrue(result);
+
+            IEnumerable<String> expectedFilePaths = System.IO.Directory.EnumerateFiles(expectedResultsPath);
+            Assert.AreNotEqual(expectedFilePaths.Count(), 0, "There were no files in the ExpectedResultsDirectory");
+
+            foreach (string path in expectedFilePaths)
+            {
+                string expectedFileName = System.IO.Path.GetFileName(path);
+                string actualPath = actualResultsPath + expectedFileName;
+                Assert.IsTrue(System.IO.File.Exists(actualPath), "The following file does not exist in Actual Results: " + actualPath);
+
+                string expectedFileString = System.IO.File.ReadAllText(path);
+                string actualFileString = System.IO.File.ReadAllText(actualPath);
+
+                Assert.AreEqual(expectedFileString, actualFileString, " *** " + expectedFileName + " *** ");
+            }
+
+            // Tear Down (only runs if all Asserts pass, so if there is a failure, I can examine the file)
+            IEnumerable<String> actualFilePaths = System.IO.Directory.EnumerateFiles(actualResultsPath);
+            foreach (string path in actualFilePaths)
+            {
+                System.IO.File.Delete(path);
+            }
+
+        }
     }
 }
